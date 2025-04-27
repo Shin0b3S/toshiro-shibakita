9d2922e71c748aa958cabc9fe058b6eb2900fb98
Shin0bOUs <ivan.fersantis@gmail.com>
2025-04-27 18:41:19 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
-
 <head>
-<title>Exemplo PHP</title>
+    <title>Exemplo PHP</title>
 </head>
 <body>
 
@@ -9,40 +8,25 @@
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
 
-
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
-
+// Configuração de banco de dados
 $servername = "54.234.153.24";
 $username = "root";
 $password = "Senha123";
 $database = "meubanco";
 
-// Criar conexão
-
-
+// Criar conexão com o banco de dados
 $link = new mysqli($servername, $username, $password, $database);
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+// Verificar conexão
+if ($link->connect_error) {
+    die("Conexão falhou: " . $link->connect_error);
 }
 
-$valor_rand1 =  rand(1, 999);
+// Gerar dados aleatórios
+$valor_rand1 = rand(1, 999);
 $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
+// Preparar e executar a consulta
+$query = $link->prepare("INSERT INTO dados (AlunoID, Nome, Sobrenome, Endere
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
-
-if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $link->error;
-}
-
-?>
-</body>
-</html>
